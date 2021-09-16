10e3e2752fa2d2d2708afcc3ceb6c66a330bdd82
pranav <pranav.s.pradeep@gmail.com>
2021-09-16 13:18:46 +0530

    Started coding on sequelize

---
+++ ORM-Node Js/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+const db = require("./src/models");
+
+
+db.sequelize.sync();  //----used in production
+//in development we may need to drop table this for that
+
+/*db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+  });*/
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to pranav application." });
+});
+//routes
+require("./src/routes/turorial.routes")(app);
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
